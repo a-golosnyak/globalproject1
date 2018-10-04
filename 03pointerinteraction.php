29d16eac02730d8b8990ca0484aea86dedf55f39
AND <andrey_g.pt@mail.ru>
2018-10-04 10:27:53 +0300

    parallax logic

--- 03pointerinteraction.php
+++ 03pointerinteraction.php
@@ -22,10 +22,13 @@
 
             function move() 
             {
-                document.getElementById('x31').style.left = 300 + 2*(X-300)/3 + 'px';
-                document.getElementById('x32').style.left = 300 + (X-300)/2 + 'px';
-                document.getElementById('x33').style.left = 300 + (X-300)/3 + 'px';
-            //   document.getElementById('x3').style.top = Y + 'px';
+                document.getElementById('x31').style.left = 300 - (X-500)/2 + 'px';
+                document.getElementById('x32').style.left = 300 - (X-500)/3 + 'px';
+                document.getElementById('x33').style.left = 300 - (X-500)/4 + 'px';
+
+                document.getElementById('x31').style.top = 300 - (Y-300)/3 + 'px';
+                document.getElementById('x32').style.top = 300 - (Y-300)/4 + 'px';
+                document.getElementById('x33').style.top = 300 - (Y-300)/5 + 'px';
             }
 
             document.addEventListener("mousemove", function (e) {
@@ -36,20 +39,24 @@
             setInterval(move, 100);
         });
     </script>
-
-      <div style="height: 100vh">
-            <body>
+    <div class="container">
+        <div style="height: 100vh">
             <br>
             <br>
             <hr>
-                <div class="draw animated fadeIn shadow m-5 p-4 w-50 h-75"> 
+            <!--    <div class="container draw animated fadeIn shadow m-5 p-4 w-50 h-75"> 
                     <div class="x3" id="x31"></div>
                     <div class="x3" id="x32"></div>
                     <div class="x3" id="x33"></div>
-                </div>
-          </body>
-      </div>
-      <!-- /Start your project here-->
+                </div>  -->
+                <ul class="container draw shadow w-50 h-50 viewport" >
+                    <li class="x3" id="x31"></li>
+                    <li class="x3" id="x32"></li>
+                    <li class="x3" id="x33"></li>
+                </ul>
+        </div>
+    </div>
+    <!-- /Start your project here-->
 <!--=============================================================================================-->
     <!-- SCRIPTS -->
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
